58d283320e25b2a15337ffd5fdc70f1648c9267c
greyglam <57892430+greyglam@users.noreply.github.com>
2022-10-21 09:43:26 +0700

    commit temp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-import { ReactReader } from "react-reader";
-
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chapter from "./components/Chapter";
+import Ebook from "./components/Ebook";
 function App() {
-  const [location, setLocation] = useState(null);
-  const locationChanged = (epubcifi) => {
-    // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
-    setLocation(epubcifi);
-  };
   return (
-    <div style={{ height: "100vh" }}>
-      <ReactReader
-        location={location}
-        locationChanged={locationChanged}
-        // url="https://react-reader.metabits.no/files/alice.epub"
-        url="/sample.epub"
-      />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/ebook" />} />
+      <Route exact path="/chapter" element={<Chapter />} />
+      <Route exact path="/ebook" element={<Ebook />} />
+    </Routes>
   );
 }
 
---
+++ src/components/Chapter.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Chapter = () => {
+  return <h1>Hello</h1>;
+};
+export default Chapter;
